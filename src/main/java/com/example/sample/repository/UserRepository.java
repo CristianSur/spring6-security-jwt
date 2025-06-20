dc31414690d5e28101a6a5757a17df0dc0a4c4a3
Cristian <suruceanu.cristian@gmail.com>
2025-06-20 21:25:43 +0300

    docker-compose.yaml

--- src/main/java/com/example/sample/repository/UserRepository.java
+++ src/main/java/com/example/sample/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByUsername(String username);
 
     Optional<UserEntity> findByEmail(String email);
+
 }
